a228e2bd3c56c0f90917caa930b38d2daf7f8dfb
FT-Key <fr4nc0t2@gmail.com>
2024-08-11 21:52:44 -0300

    Se corrige la url del cors para acceder desde el frontend deployeado en netlify y se agrega estilos y funcionalidad script al index

--- src/server/app.js
+++ src/server/app.js
@@ -27,18 +27,23 @@ class Server {
       res.send(`
         <!DOCTYPE html>
         <html lang="en">
+        
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Server</title>
           <style>
+            :root {
+              --color: #df1600;
+              --animation-duration: 2s;
+            }
+        
             * {
               margin: 0;
               padding: 0;
               box-sizing: border-box;
-              --color: #df1600;
             }
-    
+        
             body {
               display: flex;
               justify-content: center;
@@ -46,110 +51,112 @@ class Server {
               min-height: 100vh;
               background: #000;
             }
-    
+        
             .loader {
               position: relative;
               width: 200px;
               height: 200px;
               overflow: hidden;
               -webkit-box-reflect: below 1px linear-gradient(transparent, #0005);
+              transition: .5s ease;
             }
-    
+        
             .loader:hover {
               background: var(--color);
               box-shadow: 0 0 5px var(--color),
                           0 0 25px var(--color),
                           0 0 50px var(--color),
                           0 0 200px var(--color);
-              transition: .3s ease;
+              transition: .5s ease;
             }
-    
+        
             .loader span {
               position: absolute;
               display: block;
             }
-    
+        
             .loader span:nth-child(1) {
               top: 0;
               left: -100%;
               width: 100%;
               height: 30px;
               background: linear-gradient(90deg, transparent, var(--color));
-              animation: animate1 1s linear infinite;
+              animation: animate1 var(--animation-duration) linear infinite;
               animation-delay: 0s;
             }
-    
+        
             @keyframes animate1 {
               0% {
                 left: -100%;
               }
-    
+        
               100% {
                 left: 100%;
               }
             }
-    
+        
             .loader span:nth-child(3) {
               bottom: 0;
               right: -100%;
               width: 100%;
               height: 30px;
               background: linear-gradient(270deg, transparent, var(--color));
-              animation: animate3 1s linear infinite;
+              animation: animate3 var(--animation-duration) linear infinite;
               animation-delay: 0s;
             }
-    
+        
             @keyframes animate3 {
               0% {
                 right: -100%;
               }
-    
+        
               100% {
                 right: 100%;
               }
             }
-    
+        
             .loader span:nth-child(2) {
               right: 0;
               top: -100%;
               height: 100%;
               width: 30px;
               background: linear-gradient(180deg, transparent, var(--color));
-              animation: animate2 1s linear infinite;
-              animation-delay: .5s;
+              animation: animate2 var(--animation-duration) linear infinite;
+              animation-delay: calc(var(--animation-duration) / 2);
             }
-    
+        
             @keyframes animate2 {
               0% {
                 top: -100%;
               }
-    
+        
               100% {
                 top: 100%;
               }
             }
-    
+        
             .loader span:nth-child(4) {
               left: 0;
               bottom: -10%;
               height: 100%;
               width: 30px;
               background: linear-gradient(360deg, transparent, var(--color));
-              animation: animate4 1s linear infinite;
-              animation-delay: .5s;
+              animation: animate4 var(--animation-duration) linear infinite;
+              animation-delay: calc(var(--animation-duration) / 2);
             }
-    
+        
             @keyframes animate4 {
               0% {
                 top: 100%;
               }
-    
+        
               100% {
                 top: -100%;
               }
             }
           </style>
         </head>
+        
         <body>
           <div class="loader">
             <span></span>
@@ -157,7 +164,21 @@ class Server {
             <span></span>
             <span></span>
           </div>
+        
+          <script>
+            const colors = ['#39FF14', '#03e9f4', '#00ff44', '#FF4500', '#FFFF33'];
+            let currentColorIndex = 0;
+        
+            function changeColor() {
+              currentColorIndex = (currentColorIndex + 1) % colors.length;
+              document.documentElement.style.setProperty('--color', colors[currentColorIndex]);
+            }
+        
+            setInterval(changeColor, 3000);
+          </script>
+        
         </body>
+        
         </html>
       `);
     });
@@ -166,7 +187,7 @@ class Server {
   middlewares() {
     // MiddleWare
     const corsOptions = {
-      origin: ['http://localhost:5173', 'https://deploy-preview-1--reactfrontendtemplates.netlify.app'], // Permitir solicitudes desde estos orígenes
+      origin: ['http://localhost:5173', 'https://reactfrontendtemplates.netlify.app'], // Permitir solicitudes desde estos orígenes
       methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
       allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
     };
